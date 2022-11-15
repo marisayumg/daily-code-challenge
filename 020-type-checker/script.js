0a5bef96bea72cc0136168ac299eeb1e6151b890
marisayumg <35278286+marisayumg@users.noreply.github.com>
2022-11-15 09:38:27 +0000

    Added textarea functionality

--- 020-type-checker/script.js
+++ 020-type-checker/script.js
@@ -1,7 +1,10 @@
 const sentenceTag = document.querySelector("textarea");
 const sentenceInput = document.querySelector(`input[type="text"]`);
+const sizeInput = document.querySelector(`input[type="range"]`);
+
 const originalText = sentenceTag.value;
 
+// display the text from text input on the big sentence on the RHS
 sentenceInput.addEventListener("keyup", function () {
   if (this.value) {
     sentenceTag.value = this.value;
@@ -10,6 +13,12 @@ sentenceInput.addEventListener("keyup", function () {
   }
 });
 
+// display the text from the textarea on the text input
 sentenceTag.addEventListener("keyup", function () {
   sentenceInput.value = this.value;
+});
+
+// change font size depending on range value
+sizeInput.addEventListener("click", function () {
+  sentenceTag.style.fontSize = this.value;
 });
