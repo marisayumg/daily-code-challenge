e02f7fee5e9f06e282926152c215feab4163b809
marisayumg <35278286+marisayumg@users.noreply.github.com>
2022-11-19 00:00:04 +0000

    added circle animation

--- 023-svg-animations/script/main.js
+++ 023-svg-animations/script/main.js
@@ -1,6 +1,10 @@
-anime({
+const CSStransforms = anime({
   targets: "#tunnel circle",
-  scale: 1.2,
+  scale: 1.1,
   direction: "alternate",
-  lop: true,
+  loop: true,
+  duration: 250,
+  easing: "easeInOutSine",
+  // delay function that loops through all the elements, their index at the max length
+  delay: (el, index) => index * 50,
 });
