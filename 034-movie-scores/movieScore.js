bbcaec622d9790a15381f2e1e52cf3bce8642a02
marisayumg <35278286+marisayumg@users.noreply.github.com>
2022-12-02 23:52:32 +0000

    Added scattermap and update on select input

--- 034-movie-scores/movieScore.js
+++ 034-movie-scores/movieScore.js
@@ -1,8 +1,40 @@
+const selectTag = document.querySelector("select");
+
 const svg = d3.select("svg");
 
 // score scale
 // prettier-ignore
 const scoreScale = d3.scaleLinear()
+    .domain([0,100])
+    .range([800, 1200])
+
+// imdb line specifications
+// prettier-ignore
+const imdbLine = d3.line()
+    .x((d, i) => {return scoreScale(d.imdb) + 80})
+    .y((d, i) => {return i * 48 + 80})
+
+// imdb Path tag (the actual element)
+// prettier-ignore
+const imdbPath = svg
+  .append("path")
+  .datum(data)
+  .attr("d", imdbLine)
+  .attr("class", "imdb")
+
+// imdb line specifications
+// prettier-ignore
+const metascoreLine = d3.line()
+    .x((d, i) => {return scoreScale(d.metascore) + 82})
+    .y((d, i) => {return i * 48 + 72})
+
+// imdb Path tag (the actual element)
+// prettier-ignore
+const metascorePath = svg
+  .append("path")
+  .datum(data)
+  .attr("d", metascoreLine)
+  .attr("class", "metascore")
 
 // prettier-ignore
 svg
@@ -19,17 +51,27 @@ const movieGroup = svg
     return `translate(80, ${i * 48 + 80})`;
   });
 
-// title
+// movie year
+// prettier-ignore
+movieGroup
+  .append("text")
+  .attr("class", "year")
+  .text((d, i) => {
+    return d.year;
+  });
+
+// movie title
 // prettier-ignore
 movieGroup
     .append("text")
+    .attr("x", 80)
     .text((d, i) => {return d.title})
 
 // imbd circle
 // prettier-ignore
 movieGroup
   .append("circle")
-  .attr("cx", (d, i) => { return d.imdb + 1100})
+  .attr("cx", (d, i) => { return scoreScale(d.imdb)})
   .attr("cy", -8)
   .attr("r", 8)
   .attr("class", "imdb")
@@ -39,7 +81,7 @@ movieGroup
 movieGroup
   .append("circle")
   .attr("cx", (d, i) => {
-    return d.metascore + 1140
+    return scoreScale(d.metascore)
   })
   .attr("cy", -8)
   .attr("r", 8)
@@ -53,3 +95,30 @@ movieGroup
   .attr("y", -30)
   .attr("width", 1280)
   .attr("height", 48)
+
+selectTag.addEventListener("change", function () {
+  data.sort((a, b) => {
+    if (this.value === "imdb") {
+      return d3.descending(a.imdb, b.imdb);
+    }
+    if (this.value === "metascore") {
+      return d3.descending(a.metascore, b.metascore);
+    }
+    if (this.value === "title") {
+      return d3.ascending(a.title, b.title);
+    }
+    if (this.value === "year") {
+      return d3.ascending(a.year, b.year);
+    }
+  });
+
+  // Tell data to update again
+  // prettier-ignore
+  movieGroup
+    .data(data, (d, i) => { return d.title })
+    .transition()
+    .duration(1000)
+    .attr("transform", (d, i) => {
+      return `translate(80, ${i * 48 + 80})`;
+    });
+});
