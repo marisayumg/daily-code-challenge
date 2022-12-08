4cfc7f395ffd0947c2229dc08cc4a86388f2f60b
marisayumg <35278286+marisayumg@users.noreply.github.com>
2022-12-08 14:48:18 +0000

    Changed order of images

--- 036-plant-slideshow/slideshow.js
+++ 036-plant-slideshow/slideshow.js
@@ -5,12 +5,34 @@ let currentSlide = 0;
 let z = 1;
 
 mainTag.addEventListener("click", function () {
+  currentSlide++;
+
   if (currentSlide > images.length - 1) {
     currentSlide = 0;
   }
 
-  currentSlide++;
   z++;
 
+  // remove the animation for the style for EVERY IMAGE
+  images.forEach((image) => {
+    image.style.animation = "";
+  });
+
   images[currentSlide].style.zIndex = z;
+  images[currentSlide].style.animation = `fadeIn 0.1s`;
+});
+
+mainTag.addEventListener("mouseenter", function () {
+  images.forEach((image) => {
+    let x = 25 * Math.floor(Math.random() * images.length) - 50;
+    let y = 25 * Math.floor(Math.random() * images.length) - 50;
+
+    image.style.transform = `translate(${x}px, ${y}px)`;
+  });
+});
+
+mainTag.addEventListener("mouseleave", function () {
+  images.forEach((image) => {
+    image.style.transform = "";
+  });
 });
