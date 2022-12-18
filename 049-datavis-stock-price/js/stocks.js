9f8d713ec80d7759d5f4820cfa538473aef52e13
marisayumg <35278286+marisayumg@users.noreply.github.com>
2022-12-18 22:38:52 +0000

    Added the data dependant tooltip

--- 049-datavis-stock-price/js/stocks.js
+++ 049-datavis-stock-price/js/stocks.js
@@ -2,9 +2,11 @@
 const svg = d3.select("svg");
 svg.attr("viewBox", "0,0, 960, 320");
 
+const apiUrl = "https://api.superhi.com/api/stocks/aapl";
+
 // API call
 // prettier-ignore
-d3.json("js/1y.json")
+d3.json(apiUrl)
     .then((data) => {
       // transform the date (which is given as a string in the API into an actual date with d3)
       const dateParse = d3.timeParse("%Y-%m-%d");
@@ -66,4 +68,62 @@ d3.json("js/1y.json")
         .datum(data)
         .attr("class", "line")
         .attr("d", line);
+    
+        const hoverGroup = svg
+            .append("g")
+            .attr("transform", "translate(-100,-100)")
+
+        hoverGroup
+            .append("rect")
+            .attr("x", -50)
+            .attr("y", -60)
+            .attr("width", 100)
+            .attr("height", 50)
+
+        hoverGroup
+            .append("circle")
+            .attr("cx", 0)
+            .attr("cy", 0)
+            .attr("r", 4)
+
+        // save to a constant as we want to change it later on
+        const stockText = hoverGroup
+            .append("text")
+            .attr("class", "stock")
+            .attr("x", 0)
+            .attr("y", -37)
+            .text("Hi")
+
+        const dateText = hoverGroup
+            .append("text")
+            .attr("class", "date")
+            .attr("x", 0)
+            .attr("y", -18)
+            .text("date")
+
+        svg.on("mousemove", function() {
+            // returns the x and y coordinates
+            const mouse = d3.mouse(this)
+            // selecting just the x from above
+            const mouseX = mouse[0]
+
+            const mouseDate = dateScale.invert(mouseX)
+            const bisector = d3.bisector((d) => { return d.date }).right
+            const dataIndex = bisector(data, mouseDate)
+            const dataPoint = data[dataIndex]
+
+            if(dataPoint) {
+                const x = dateScale(dataPoint.date);
+                const y = stockScale(dataPoint.stock);
+                const timeFormat = d3.timeFormat("%Y-%b-%d");
+                const dateFormatted = timeFormat(dataPoint.date);
+                stockText.text(dataPoint.stock);
+                dateText.text(dateFormatted);
+                hoverGroup.attr("transform", `translate(${x}, ${y})`)
+            }
+        })
+
+        svg.on("mouseout", function() {
+            hoverGroup.attr("transform", "translate(-999, -999)")
+        })
     })
