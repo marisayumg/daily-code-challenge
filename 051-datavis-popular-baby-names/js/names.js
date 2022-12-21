15a73666711bba1bbff5fc09264148a2db94ea6b
marisayumg <35278286+marisayumg@users.noreply.github.com>
2022-12-21 22:41:05 +0000

    Added scales

--- 051-datavis-popular-baby-names/js/names.js
+++ 051-datavis-popular-baby-names/js/names.js
@@ -6,8 +6,7 @@ const svg = d3.select("svg");
 
 // prettier-ignore
 svg
-    .attr("width", 960)
-    .attr("height", 540)
+    .attr("viewBox", "0 0 1200 480")
 
 // prettier-ignore
 const pathsGroup = svg
@@ -15,23 +14,35 @@ const pathsGroup = svg
     .attr("class", "paths")
 
 // prettier-ignore
-const rankScale = d3.scaleLinear()
+const rankScale = d3.scalePow()
     .domain([1, 1000])
-    .range([20, 500])
+    .range([20, 440])
+    .exponent(0.25)
 
 // prettier-ignore
 const dateScale = d3.scaleLinear()
     .domain([1880, 2010])
-    .range([80, 915])
+    .range([90, 1100])
 
 // prettier-ignore
 const rankAxis = d3.axisLeft(rankScale)
-const dateAxis = d3.axisBottom(dateScale);
+    .tickValues([1, 5, 10, 25, 50, 75, 100, 500, 750, 1000])
+// prettier-ignore
+const dateAxis = d3.axisBottom(dateScale)
+.tickFormat((d, i) => { return d + "'s"})
+.tickPadding(8)
 
 // prettier-ignore
 const line = d3.line()
     .x((d, i) => { return dateScale(1880 + 10 * i) })
     .y((d, i) => { return rankScale(d) })
+    .defined((d, i) => { return d != 0 })
+    .curve(d3.curveCardinal.tension(0.5))
+
+// prettier-ignore
+const flatLine = d3.line()
+    .x((d, i) => { return dateScale(1880 + 10 * i) })
+    .y((d, i) => { return rankScale(1000) })
     .defined((d, i) => { return d != 0 })
     .curve(d3.curveCardinal.tension(0.5))
 
@@ -44,7 +55,7 @@ svg
 // prettier-ignore
 svg
     .append("g")
-    .attr("transform", "translate(0, 520)")
+    .attr("transform", "translate(0, 440)")
     .call(dateAxis)
 
 // search name function
@@ -66,11 +77,21 @@ const searchName = (name) => {
         .enter()
         .append("path")
         .attr("class", (d, i) => { return d.sex })
+        .attr("d", (d, i) => { return flatLine(d.rank) })
+        .style("opacity", 0)
+        .transition()
+        .duration(500)
+        .style("opacity", 1)
         .attr("d", (d, i) => { return line(d.rank) })
 
     // prettier-ignore
     lines
         .exit()
+        .style("opacity", 1)
+        .transition()
+        .duration(500)
+        .style("opacity", 0)
+        .attr("d", (d, i) => { return flatLine(d.rank) })
         .remove()
   } else {
     alert(`No results for ${name}`);
