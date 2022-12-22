8b5ac717056de9ebb941f93f9e3c45096ba6c667
marisayumg <35278286+marisayumg@users.noreply.github.com>
2022-12-22 21:09:21 +0000

    Initial setup

---
+++ 052-P5-wall-ball/js/ball.js
@@ -0,0 +1,12 @@
+// runs once
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+// runs every frame
+function draw() {
+  background("black");
+  fill("white");
+  noStroke();
+  circle(300, 200, 300);
+}
