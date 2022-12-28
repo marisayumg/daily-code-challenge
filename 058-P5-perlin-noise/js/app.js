3527c854e85060969d5b9b2f9429034c102c7007
marisayumg <35278286+marisayumg@users.noreply.github.com>
2022-12-28 21:18:48 +0000

    Update app.js

--- 058-P5-perlin-noise/js/app.js
+++ 058-P5-perlin-noise/js/app.js
@@ -1,26 +1,40 @@
 let time;
 let noiseLevel;
+let totalX;
+let totalY;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background("black");
   time = 0;
-  noiseLevel = 0.01;
+  noiseLevel = 0.005;
+  totalX = windowWidth;
+  totalY = windowHeight;
+
+  colorMode(HSL, 100);
+
+  for (let x = 0; x < totalX; x++) {
+    for (let y = 0; y < totalY; y++) {
+      // let number = random();
+      // let number = noise(time * noiseLevel);
+      let number = noise(x * noiseLevel, y * noiseLevel);
+
+      let finalColor = color(number * 100, 100, 50);
+
+      set(x, y, finalColor);
+    }
+  }
+
+  updatePixels();
 }
 
 function draw() {}
 
 function mouseDragged() {
-  // let number = random();
-  // let number = noise(time * noiseLevel);
-  let number = noise(mouseX * noiseLevel, mouseY * noiseLevel);
   let size = number * 50;
-  let colour1 = color("white");
-  let colour2 = color("#2727e6");
-  let colourMix = lerpColor(colour1, colour2, number);
 
   noStroke();
-  fill(colourMix);
+  fill(finalColor);
   circle(mouseX, mouseY, size);
 
   time = time + 1;
