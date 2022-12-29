4caa56469772aca939e68e2548d92948d2f3e857
marisayumg <35278286+marisayumg@users.noreply.github.com>
2022-12-29 11:59:30 +0000

    Added final noise and pattern

--- 058-P5-perlin-noise/js/app.js
+++ 058-P5-perlin-noise/js/app.js
@@ -1,41 +1,39 @@
-let time;
 let noiseLevel;
 let totalX;
 let totalY;
+let density;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background("black");
-  time = 0;
-  noiseLevel = 0.005;
+}
+
+function draw() {
+  noiseLevel = 0.001;
   totalX = windowWidth;
   totalY = windowHeight;
+  density = 10;
 
-  colorMode(HSL, 100);
+  // colorMode(HSL, 100);
+  let color1 = color("#2727e6");
+  let color2 = color("#fff");
 
   for (let x = 0; x < totalX; x++) {
     for (let y = 0; y < totalY; y++) {
       // let number = random();
       // let number = noise(time * noiseLevel);
       let number = noise(x * noiseLevel, y * noiseLevel);
+      let reducedNoise = number * density - Math.floor(number * density);
 
-      let finalColor = color(number * 100, 100, 50);
+      let d = map(reducedNoise, 0, 0.5, 0, 1);
+      if (reducedNoise > 0.5) {
+        d = map(reducedNoise, 0.5, 1, 1, 0);
+      }
 
+      let finalColor = lerpColor(color2, color1, d);
       set(x, y, finalColor);
     }
   }
 
   updatePixels();
-}
-
-function draw() {}
-
-function mouseDragged() {
-  let size = number * 50;
-
-  noStroke();
-  fill(finalColor);
-  circle(mouseX, mouseY, size);
-
-  time = time + 1;
 }
