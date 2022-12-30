bf57edd1591fb7009b43d7eebc4c1adf8d711eee
marisayumg <35278286+marisayumg@users.noreply.github.com>
2022-12-30 11:38:08 +0000

    Added project files

---
+++ 060-P5-flying-bubbles/js/word.js
@@ -0,0 +1,20 @@
+class Word {
+  constructor(x, y, size) {
+    let randomAngle = random(-0.25 * PI, 0.25 * PI);
+
+    this.position = createVector(x, y);
+    this.speed = createVector(0, -5);
+    this.speed = this.speed.rotate(randomAngle);
+    this.size = size;
+  }
+
+  move() {
+    this.position = this.position.add(this.speed);
+    this.size = constrain(this.size - 1, 0, 50);
+  }
+
+  draw() {
+    this.move();
+    circle(this.position.x, this.position.y, this.size);
+  }
+}
