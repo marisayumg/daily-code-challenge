286f920cce87a649c3127e78a212148de7abecc0
marisayumg <35278286+marisayumg@users.noreply.github.com>
2023-01-07 23:25:22 +0000

    Added images fade in on page leave and enter

--- 067-js-page-transitions-2/js/scripts.js
+++ 067-js-page-transitions-2/js/scripts.js
@@ -1,3 +1,5 @@
+const bodyTag = document.querySelector("body");
+
 const runScripts = () => {
   const headers = document.querySelectorAll("h2, h3");
   const imageHolders = document.querySelectorAll("div.image");
@@ -34,15 +36,67 @@ barba.init({
   transitions: [
     {
       name: "switch",
-      leave({ current, next, trigger }) {},
-      enter({ current, next, trigger }) {
-        runScripts();
+      once({ current, next, trigger }) {
         return new Promise((resolve) => {
-          resolve();
+          const images = document.querySelectorAll("img");
+          gsap.set(next.container, { opacity: 0 });
+          imagesLoaded(images, () => {
+            const timeline = gsap.timeline({
+              onComplete() {
+                resolve();
+              },
+            });
+            timeline
+              .set(next.container, { opacity: 0 })
+              .to(next.container, { opacity: 1, delay: 1 });
+          });
+        });
+      },
+      leave({ current, next, trigger }) {
+        return new Promise((resolve) => {
+          const timeline = gsap.timeline({
+            onComplete() {
+              current.container.remove();
+              resolve();
+            },
+          });
+          timeline
+            .to("header", { y: "-100%" })
+            .to("footer", { y: "100$" })
+            .to(current.container, { opacity: 0 });
+        });
+      },
+      enter({ current, next, trigger }) {
+        return new Promise((resolve) => {
+          window.scrollTo({
+            top: 0,
+            behavior: "smooth",
+          });
+          const timeline = gsap.timeline({
+            onComplete() {
+              runScripts();
+              resolve();
+            },
+          });
+          timeline
+            .set(next.container, { opacity: 0 })
+            .to("header", { y: "0%" })
+            .to("footer", { y: "0%" })
+            .to(next.container, { opacity: 1 });
         });
       },
     },
   ],
-  views: [],
+  views: [
+    {
+      namespace: "product",
+      beforeEnter() {
+        bodyTag.classList.add("product");
+      },
+      afterLeave() {
+        bodyTag.classList.remove("product");
+      },
+    },
+  ],
   debug: true,
 });
