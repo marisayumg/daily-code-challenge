05d8ed6cdccea320c0b503858d435df2b487ba07
marisayumg <35278286+marisayumg@users.noreply.github.com>
2023-01-09 20:53:25 +0000

    Added drawing functionality with canvas

--- 069-js-festival-lineup/scripts.js
+++ 069-js-festival-lineup/scripts.js
@@ -1,6 +1,8 @@
 const cursorTag = document.querySelector("div.cursor");
 const canvasTag = document.querySelector("canvas.in");
 
+let isMouseDown = false;
+
 const growCursor = function () {
   cursorTag.classList.add("is-down");
 };
@@ -22,18 +24,49 @@ const setupCanvas = function (canvas) {
   canvas.height = h * dpi;
   canvas.style.width = w + "px";
   canvas.style.height = h + "px";
+
+  const context = canvas.getContext("2d");
+  context.scale(dpi, dpi);
+  context.fillStyle = "blue";
+  context.strokeStyle = "blue";
+  context.lineWidth = 80;
+  context.lineCap = "round";
+  context.lineJoin = "round";
+};
+
+const startDraw = function (canvas, x, y) {
+  const context = canvas.getContext("2d");
+  const colors = ["blue", "red", "pink", "yellow", "orange"];
+  const randomNum = Math.floor(Math.random() * colors.length);
+
+  context.strokeStyle = colors[randomNum];
+
+  context.moveTo(x, y);
+  context.beginPath();
+};
+
+const moveDraw = function (canvas, x, y) {
+  const context = canvas.getContext("2d");
+  if (isMouseDown) {
+    context.lineTo(x, y);
+    context.stroke();
+  }
 };
 
 setupCanvas(canvasTag);
 
-document.addEventListener("mousedown", function () {
+document.addEventListener("mousedown", function (event) {
+  isMouseDown = true;
   growCursor();
+  startDraw(canvasTag, event.pageX, event.pageY);
 });
 
 document.addEventListener("mouseup", function () {
+  isMouseDown = false;
   shrinkCursor();
 });
 
 document.addEventListener("mousemove", function (event) {
   moveCursor(event.pageX, event.pageY);
+  moveDraw(canvasTag, event.pageX, event.pageY);
 });
