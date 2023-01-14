b5fc0d4712e2d307a3ce5ae078c01b51fe14412d
marisayumg <35278286+marisayumg@users.noreply.github.com>
2023-01-14 23:45:49 +0000

    Added mobile responsive

--- 074-js-seamstress/js/slides.js
+++ 074-js-seamstress/js/slides.js
@@ -11,21 +11,58 @@ slides.forEach((slide) => {
     image.style.zIndex = z;
   });
 
-  const timeline = gsap.timeline();
+  gsap.set(images, { opacity: 0 });
 
-  //prettier-ignore
-  timeline
-    .set(images, { y: "500%", rotation: () => {
+  imagesLoaded(images, function () {
+    const timeline = gsap.timeline();
+
+    //prettier-ignore
+    timeline
+    .set(images, { x: () => {
+        return 500 * Math.random() - 250
+    },
+    y: "500%",
+    rotation: () => {
         return 40 * Math.random() - 20
-    }})
-    .to(images, { y: 0, stagger: -0.25 })
+    },
+    opacity: 1
+    })
+    .to(images, { x: 0, y: 0, stagger: -0.25 })
     .to(images, { rotation: () => {
         return 16 * Math.random() - 8
-    } })
+    }})
+  });
 
   slide.addEventListener("click", function () {
     z--;
-    images[currentSlide].style.zIndex = z;
+
+    let direction = "150%";
+    let midAngle = 15;
+
+    if (Math.random() > 0.5) {
+      direction = "-150%";
+      midAngle = -15;
+    }
+
+    const currentImage = images[currentSlide];
+
+    const flipTimeline = gsap.timeline();
+
+    //prettier-ignore
+    flipTimeline
+        .set(currentImage, { x: 0 })
+        .to(currentImage, {
+            x: direction,
+            rotation: midAngle
+        })
+        .set(currentImage, { zIndex: z })
+        .to(currentImage, { 
+            x: 0,
+            rotation: () => {
+                return 16 * Math.random() - 8
+            }
+        })
+
     currentSlide++;
     currentSlide = currentSlide % images.length;
   });
