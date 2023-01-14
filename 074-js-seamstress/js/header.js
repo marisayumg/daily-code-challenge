f36887fc597db9a9b1d46862af2f4fe62908673f
marisayumg <35278286+marisayumg@users.noreply.github.com>
2023-01-14 22:56:05 +0000

    Added image carousel animation

---
+++ 074-js-seamstress/js/header.js
@@ -0,0 +1,28 @@
+const carousels = document.querySelectorAll("header h1, header h2");
+
+const fadeInTimeline = gsap.timeline();
+
+//prettier-ignore
+fadeInTimeline
+    .set(carousels, { opacity: 0 })
+    .to(carousels, { opacity: 1, delay: 1, stagger: 1 })
+
+// we want a different timeline per carousel
+carousels.forEach((carousel) => {
+  const spanTag = carousel.querySelector("span");
+  const spanWidth = spanTag.clientWidth;
+
+  for (let i = 0; i < 20; i++) {
+    carousel.appendChild(spanTag.cloneNode(true));
+  }
+
+  const movementTimeline = gsap.timeline({
+    // repeat infinitely
+    repeat: -1,
+  });
+
+  //prettier-ignore
+  movementTimeline
+    .set(carousel, { x: 0 })
+    .to(carousel, { x: spanWidth * -1, duration: 6, ease: "linear" })
+});
--- 074-js-seamstress/js/slides.js
+++ 074-js-seamstress/js/slides.js
@@ -11,6 +11,18 @@ slides.forEach((slide) => {
     image.style.zIndex = z;
   });
 
+  const timeline = gsap.timeline();
+
+  //prettier-ignore
+  timeline
+    .set(images, { y: "500%", rotation: () => {
+        return 40 * Math.random() - 20
+    }})
+    .to(images, { y: 0, stagger: -0.25 })
+    .to(images, { rotation: () => {
+        return 16 * Math.random() - 8
+    } })
+
   slide.addEventListener("click", function () {
     z--;
     images[currentSlide].style.zIndex = z;
